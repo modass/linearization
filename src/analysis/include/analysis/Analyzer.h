ca2721644219dc793033278329651288c3a1f959
Stefan Schupp <stefan.schupp@tuwien.ac.at>
2022-10-20 12:42:21 +0200

    add analysis sublibrary

---
+++ src/analysis/include/analysis/Analyzer.h
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2022 Stefan Schupp.
+ * This file is part of the linearization project.
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/*
+ * Created by Stefan Schupp <stefan.schupp@tuwien.ac.at> on 20.10.22.
+ */
+
+#ifndef LINEARIZATION_ANALYZER_H
+#define LINEARIZATION_ANALYZER_H
+
+#include "types.h"
+#include <hypro/algorithms/reachability/Reach.h>
+#include <hypro/datastructures/HybridAutomaton/HybridAutomaton.h>
+
+namespace analysis
+{
+
+/**
+ * Class that wraps reachability analysis for linear hybrid systems implemented
+ * in HyPro
+ */
+template<typename Representation>
+class Analyzer
+{
+    using Number = typename Representation::NumberType;
+public:
+    /**
+     * Performs reachability analysis
+     */
+    void run();
+
+protected:
+    hypro::HybridAutomaton<Number> mAutomaton;
+};
+
+} // namespace analysis
+
+#include "Analyzer.tpp"
+
+#endif // LINEARIZATION_ANALYZER_H
---
+++ src/analysis/include/analysis/types.h
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2022 Stefan Schupp.
+ * This file is part of the linearization project.
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/*
+ * Created by Stefan Schupp <stefan.schupp@tuwien.ac.at> on 20.10.22.
+ */
+
+#ifndef LINEARIZATION_TYPES_H
+#define LINEARIZATION_TYPES_H
+
+#include <hypro/util/adaptions_carl/adaptions_includes.h> // required for mpq_class and others
+
+namespace analysis {
+
+using Rational = mpq_class;
+using Float = double;
+
+} // namespace hypro
+
+#endif // LINEARIZATION_TYPES_H
